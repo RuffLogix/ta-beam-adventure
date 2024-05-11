ac266b8ba23c41b9c3390e86d9ed15a34d4918d6
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-05-11 16:14:59 +0700

    add: utils

---
+++ src/utils/DayNightLight.java
@@ -0,0 +1,38 @@
+package utils;
+
+import javafx.animation.FadeTransition;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+import view.ViewManager;
+
+public class DayNightLight extends Pane {
+    private FadeTransition toNightTransition;
+    private FadeTransition toDayTransition;
+    public DayNightLight() {
+            setPrefSize(ViewManager.WINDOW_WIDTH*20, ViewManager.WINDOW_HEIGHT*20);
+            setBackground(new Background(new BackgroundFill(Color.rgb(0, 0, 50, 0.55), null, null)));
+
+            toNightTransition = new FadeTransition();
+            toNightTransition.setNode(this);
+            toNightTransition.setDuration(Duration.seconds(3));
+            toNightTransition.setFromValue(0);
+            toNightTransition.setToValue(1);
+
+            toDayTransition = new FadeTransition();
+            toDayTransition.setNode(this);
+            toDayTransition.setDuration(Duration.seconds(3));
+            toDayTransition.setFromValue(1);
+            toDayTransition.setToValue(0);
+    }
+
+    public void toNight() {
+        toNightTransition.play();
+    }
+
+    public void toDay() {
+        toDayTransition.play();
+    }
+}
